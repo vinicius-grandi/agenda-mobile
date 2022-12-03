76844aba0c374e2e9e99fdff68e0f0632059d7ba
vinicius-grandi <viniciusggrandi@gmail.com>
2022-12-03 17:53:27 -0300

    create notification

--- app/src/main/java/com/vinicius_grandi/agenda/MainActivity.java
+++ app/src/main/java/com/vinicius_grandi/agenda/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vinicius_grandi.agenda;
 
+import android.app.Notification;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -11,6 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -47,6 +51,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        notifyMe("created");
+    }
+
+    private void notifyMe(String message) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NotificationChannelCompat.DEFAULT_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("hello")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        Notification notification = builder.build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(1, notification);
     }
 
     @Override
@@ -69,14 +87,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (id == R.id.action_toggle_theme) {
-            View backgroundView = findViewById(R.id.nav_host_fragment_content_main);
-            Drawable background = backgroundView.getBackground();
-            int previousColor = Color.TRANSPARENT;
-            if (background instanceof ColorDrawable)
-                previousColor = ((ColorDrawable) background).getColor();
-            int black = getIntColor(R.color.black);
-            int color = previousColor == black ? getIntColor(R.color.white) : black;
-            backgroundView.setBackgroundColor(color);
+            toggleTheme();
         }
 
         if (id == R.id.action_exit_app) {
@@ -84,6 +95,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleTheme() {
+        View backgroundView = findViewById(R.id.nav_host_fragment_content_main);
+        Drawable background = backgroundView.getBackground();
+        int previousColor = Color.TRANSPARENT;
+        if (background instanceof ColorDrawable)
+            previousColor = ((ColorDrawable) background).getColor();
+        int black = getIntColor(R.color.black);
+        int color = previousColor == black ? getIntColor(R.color.white) : black;
+        backgroundView.setBackgroundColor(color);
     }
 
     private int getIntColor(int colorId) {
@@ -96,5 +118,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        notifyMe("resumed");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        notifyMe("stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        notifyMe("destroyed");
+        super.onDestroy();
     }
 }
